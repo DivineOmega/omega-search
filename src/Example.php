6cdbbd84fbccc4f6765e5fa26dc8d625cb2c18b8
Jordan Hall <jordan.hall@rapidweb.biz>
2017-08-08 13:30:36 +0100

    Added caching example

--- src/Example.php
+++ src/Example.php
@@ -1,12 +1,25 @@
 <?php
+require __DIR__.'/../vendor/autoload.php';
+
 use \RapidWeb\Search\Search;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use Cache\Adapter\Filesystem\FilesystemCachePool;
+
+if (!class_exists('Cache\Adapter\Filesystem\FilesystemCachePool')) {
+    die('This example requires the `FilesystemCachePool` class. Install it with `composer require cache/filesystem-adapter`.'.PHP_EOL);
+}
+
+$filesystemAdapter = new Local(__DIR__.'/');
+$filesystem = new Filesystem($filesystemAdapter);
+$cacheItemPool = new FilesystemCachePool($filesystem);
 
 $pdo = new PDO('mysql:dbname=database_name;host=127.0.0.1', 'username', 'password');
 
 $search = new Search;
 
 $search->setDatabaseConnection($pdo)
-//     ->setCache($cacheItemPool, 60*60*24)
+       ->setCache($cacheItemPool, 60*60*24)
        ->setTable('products')
        ->setPrimaryKey('product_groupid')
        ->setFieldsToSearch(['product_name', 'product_description', 'product_seokeywords'])
